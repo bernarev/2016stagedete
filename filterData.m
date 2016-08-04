7e7e8ad8f46483694fdf7c48190c6c76bc1317ea
Evandro Bernardes <s.evandro@hotmail.com>
2016-08-04 10:32:18 +0200

    small filterData.m doc correction

--- filterData.m
+++ filterData.m
@@ -24,7 +24,7 @@
 %		-- none	--		
 %
 % 	-> Other dependencies:  
-%		-- none	--		
+%		- selectFilterData.m	
 %									 
 %	-> Created by Evandro Bernardes	 								 
 %		- at ENSTA Bretagne (Brest, Britanny, France)							 								 
